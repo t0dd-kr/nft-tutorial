0fc3ec0dd2f3c756047dbaca39c0606b6afa11a2
t0dd <swjasontodd@gmail.com>
2021-12-26 23:57:02 +0900

    nft mint 스크립트 추가

---
+++ scripts/mint.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const { API_URL, PRIVATE_KEY, PUBLIC_KEY } = process.env;
+const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
+const web3 = createAlchemyWeb3(API_URL);
+
+const contract = require("../artifacts/contracts/MyNFT.sol/MyNFT.json");
+
+const contractAddress = '0xf36449bde642136cC569040C34BB7756B70eb2E1'
+const nftContract = new web3.eth.Contract(contract.abi, contractAddress)
+
+
+async function mintNFT(tokenURI) {
+  const nonce = await web3.eth.getTransactionCount(PUBLIC_KEY, 'latest'); //get latest nonce
+
+  //the transaction
+  const tx = {
+    'from': PUBLIC_KEY,
+    'to': contractAddress,
+    'nonce': nonce,
+    'gas': 500000,
+    'maxPriorityFeePerGas': 1999999987,
+    'data': nftContract.methods.mintNFT(PUBLIC_KEY, tokenURI).encodeABI()
+  };
+
+  const signedTx = await web3.eth.accounts.signTransaction(tx, PRIVATE_KEY);
+  const transactionReceipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
+  
+  console.log(`Transaction receipt: ${JSON.stringify(transactionReceipt)}`);
+}
+
+mintNFT("https://gateway.pinata.cloud/ipfs/QmXnAZzw7B5XG8f6Wf6vcMLErxNoVdy1zUbdFLNWxyC75Z");
